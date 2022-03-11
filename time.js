eb200bb2e5e98244030800084279042a05abc485
masa <79982360+masatojo@users.noreply.github.com>
2022-03-11 22:55:29 +0900

    Update time.js

--- time.js
+++ time.js
@@ -45,25 +45,6 @@ window.addEventListener('load', function(){
 	});
 });
 
-window.onload = function() {
-    consolog.log("test");
-    var url = "https://worldtimeapi.org/api/timezone/Asia/Tokyo";
-    let r = new XMLHttpRequest();
-    r.open('GET', url);
-    r.responseType = 'json';
-    r.send();
-
-    r.onreadystatechange = () => {
-        if (r.readyState == 4 && r.status == 200) {
-            const jsonStr = JSON.stringify(r.response);
-            const obj = JSON.parse(jsonStr);
-            var time = document.getElementById("time");
-            var date = new Date(obj["unixtime"] * 1000);//付け足したやつなので変になったら消しておいてください
-            //time.innerHTML = obj["unixtime"]*;
-            time.innerHTML= date
-        }
-    }
-}
 
   window.userAddress = null;
     window.onload = async () => {
