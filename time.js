1f7527b44930f10a45179c00fa8a68fa46c52737
seiya0818 <seiya_minecraft@yahoo.co.jp>
2022-03-12 11:08:34 +0900

    Update time.js

--- time.js
+++ time.js
@@ -38,7 +38,7 @@ async function getDate(){
     r.send();
     console.log("呼び出し");
 
-    r.onreadystatechange = async function() => {
+    r.onreadystatechange = async() => {
         if (r.readyState == 4 && r.status == 200) {
             const jsonStr = JSON.stringify(r.response);
             const obj = JSON.parse(jsonStr);
