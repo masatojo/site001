2fdf20f6ab1d81bb433abbfe9d2b7bb9356750ec
seiya0818 <seiya_minecraft@yahoo.co.jp>
2022-03-12 11:34:40 +0900

    Update time.js

--- time.js
+++ time.js
@@ -1,32 +1,32 @@
 //テストコード
-window.addEventListener('load', async function(){
+window.addEventListener('load', async function () {
     var date = await getDate();
     console.log(date);
-    
+
     //Canvas
     var image = new Image();
-	image.src = 'image002.png';
-	image.onload = (function () {
-		//画像ロードが完了してからキャンバスの準備をする
-		var canvas = document.getElementById("testCanvas");
-		var ctx = canvas.getContext('2d');
-		//キャンバスのサイズを画像サイズに合わせる
-		canvas.width = image.width;
-		canvas.height = image.height;
-		//キャンバスに画像を描画（開始位置0,0）
-		ctx.drawImage(image, 0, 0);
-        	ctx.font = '32px serif';
-		ctx.fillStyle = '#404040';
-    		//文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
-    		ctx.textBaseline = 'center';
-    		ctx.textAlign = 'center';
-    		//座標を指定して文字を描く（座標は画像の中心に）
-    		var x = (canvas.width / 2);
-    		var y = (canvas.height / 2);
-		console.log(canvas.width / 2);
-		console.log(canvas.height / 2);
-    		ctx.fillText(date, x, y);
-	});
+    image.src = 'image002.png';
+    image.onload = (function () {
+        //画像ロードが完了してからキャンバスの準備をする
+        var canvas = document.getElementById("testCanvas");
+        var ctx = canvas.getContext('2d');
+        //キャンバスのサイズを画像サイズに合わせる
+        canvas.width = image.width;
+        canvas.height = image.height;
+        //キャンバスに画像を描画（開始位置0,0）
+        ctx.drawImage(image, 0, 0);
+        ctx.font = '32px serif';
+        ctx.fillStyle = '#404040';
+        //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
+        ctx.textBaseline = 'center';
+        ctx.textAlign = 'center';
+        //座標を指定して文字を描く（座標は画像の中心に）
+        var x = (canvas.width / 2);
+        var y = (canvas.height / 2);
+        console.log(canvas.width / 2);
+        console.log(canvas.height / 2);
+        ctx.fillText(date, x, y);
+    });
 });
 
 function getDate() {
